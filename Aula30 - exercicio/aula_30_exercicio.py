57793db986bef653cdb2189730b398c105319245
Mateus Ceni <mateus.ceni@gmail.com>
2022-04-09 14:14:03 -0300

    Aulas

--- Aula30 - exercicio/aula_30_exercicio.py
+++ Aula30 - exercicio/aula_30_exercicio.py
@@ -2,11 +2,17 @@
 AULA 30 - EXERCICIO DE REPETIÇÃO  - FOR / WHILE - 08/04/22
 '''
 
+#USEI o WHILE
 
 contador = 0
-contador2 = -10
+contador2 = - 10
 while (contador < 9):
-    print(contador, contador2)
+    print(contador, abs(contador2))
     contador = contador + 1
     contador2 = contador2 + 1
 
+print('___________________________')
+#Resolução UDEMY - com o FOR... ':|' - bom a ideia era ter esse resultado!
+
+for p, r in enumerate(range(10, 1,-1)):
+    print(p, r)
