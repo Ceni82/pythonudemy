c9e058c8ac0347c9030f3e2b0656884bf7ca99ee
Mateus Ceni <mateus.ceni@gmail.com>
2022-04-08 23:57:24 -0300

    Aulas

---
+++ Aula30 - exercicio/aula_30_exercicio.py
@@ -0,0 +1,12 @@
+'''
+AULA 30 - EXERCICIO DE REPETIÇÃO  - FOR / WHILE - 08/04/22
+'''
+
+
+contador = 0
+contador2 = -10
+while (contador < 9):
+    print(contador, contador2)
+    contador = contador + 1
+    contador2 = contador2 + 1
+
